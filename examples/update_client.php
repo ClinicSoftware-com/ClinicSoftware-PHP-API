4cae127bef43f201e545e01aa4c6d684a202bc42
Alexandru ClinicSoftware.com <82223168+clinicsoftware@users.noreply.github.com>
2023-11-16 13:35:06 +0000

    Update update_client.php
    updating custom fields

--- examples/update_client.php
+++ examples/update_client.php
@@ -25,12 +25,16 @@ $data['email']           = 'apiclient9@domain.com'; // must be valid and unique
 // set this field if you need to change the password
 $data['password']        = '1234';  // plain password containing at least 4 characters (not counting spaces), hashed on server side
 
-$data['sex']             = 'm'; // gender, possible values: 'm', 'f', 'not_set'
-$data['dob']             = '2000-01-01'; // YYYY-MM-DD format
-$data['discount_value']  = 5.56; // float, global client discount
-$data['notes']           = 'test notes';
-$data['salon_id']        = 0;  // if value > 0 then salon id is verified
-$data['courses_barcode'] = ''; // valid and unique EAN8/EAN13 barcode
+$data['sex']             = 'm'; // gender, possible values: 'm', 'f', 'not_set' 
+$data['dob']             = '2000-01-01'; // YYYY-MM-DD format 
+$data['discount_value']  = 5.56; // float, global client discount 
+$data['notes']           = 'test notes'; 
+$data['salon_id']        = 0;  // if value > 0 then salon id is verified 
+$data['courses_barcode'] = ''; // valid and unique EAN8/EAN13 barcode 
+
+// set this field if you need to use custom fields 
+$data['custom_field14'] = 'string';
+$data['custom_field14'] = ['yes', 'maybe', 'etc'];
 
 // returns null, check $api->getLastStatus() == 'ok' to verify if operation succeeded
 
